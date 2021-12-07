7c4a0a4e249e8d7eab8fc01f6d87fc45c6230010
Tomrm18 <Tomrm18x@gmail.com>
2021-12-07 21:42:54 +1100

    updated project descriptions

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -8,9 +8,9 @@ const Projects:FC = () => {
     <section className="projects-container" id="projects">
       <h2 className="projects-title">Projects</h2>
       <div className="projects-grid">
-        <Project title={"Locara"} imgSrc={locaraImg} desc={"Car Subscription Service, search cars from database."} tech={["React", "Next JS", "Sass", "Typescript", "Mongodb"]} github={"https://github.com/Tomrm18/locara-project"} website={"https://locara-project-serverless.vercel.app/"}/>
+        <Project title={"Locara"} imgSrc={locaraImg} desc={"Car Subscription Service, query from up to 225 different cars from mongodb database."} tech={["React", "Next JS", "Sass", "Typescript", "Mongodb"]} github={"https://github.com/Tomrm18/locara-project"} website={"https://locara-project-serverless.vercel.app/"}/>
 
-        <Project title={"Solent"} imgSrc={solentImg} desc={"Freelance Developer Platform, heavily inspired by Fiverr and Upwork."} tech={["React", "Firebase", "JavaScript"]} github={"https://github.com/Tomrm18/odin-js-final-project"} website="/" />
+        <Project title={"Solent"} imgSrc={solentImg} desc={"Freelance Developer Platform, heavily inspired by Fiverr and Upwork, authentication handled via Firebase"} tech={["React", "Firebase", "JavaScript"]} github={"https://github.com/Tomrm18/odin-js-final-project"} website="/" />
       </div>
     </section>
   )
